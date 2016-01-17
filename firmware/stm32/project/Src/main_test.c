85c4f47b3c79fafd29e683a23945958109a3ab27
norayn <ras314zday@gmail.com>
2016-01-17 22:33:40 +0300

    start stm32 firmware

---
+++ firmware/stm32/project/Src/main_test.c
@@ -0,0 +1,38 @@
+#include "stm32f10x.h"
+ 
+void InitAll(void)
+{
+  //GPIO structure used to initialize port
+  GPIO_InitTypeDef GPIO_InitStructure;
+  //Enable clock on APB2 pripheral bus where button and LEDs are connected
+  RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOB,  ENABLE);
+  //select pins to initialize LED
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+  //select output push-pull mode
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  //highest speed available
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init( GPIOB, &GPIO_InitStructure);
+}
+
+
+ void Delay(__IO uint32_t nCount)
+{
+  for(; nCount != 0; nCount--);
+}
+
+
+int main(void)
+{
+InitAll();
+ 
+while(1)
+	{
+		Delay(500000);
+   	GPIOB->BSRR =GPIO_BSRR_BS11;		// ????????? ??? 3.  GPIO_BSRR_BS3 ??? ????????
+		Delay(500000);
+   	GPIOB->BSRR =GPIO_BSRR_BR11;		// ???????? ??? 3. 
+	}
+
+
+}	
